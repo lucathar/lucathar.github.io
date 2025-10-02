788ab787e544f736339e3a67ad2e254b526d4442
Dr Luciferous <drthedad@tutanota.de>
2025-10-02 21:59:05 +0630

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://celestialdocs.hyperoot.dev",
+  site: "https://lucathar.github.io",
   markdown: {
     smartypants: true,
     syntaxHighlight: "shiki",
